32c746737d1aae84a8a8e2ca04dc898ff1ea663c
abhijit <abhijitlimited1@gmail.com>
2024-09-26 22:03:35 +0530

    major code changes

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -11,25 +11,29 @@ function Project() {
         Developeer,here are a few of my projects
       </p>
       <div className={styles.projects}>
-        {data.map((items, index) => {
-          return (
-            <div className={styles.project} key={index}>
-              <img
-                src={items.projectImg}
-                alt={items.projectName}
-                className={styles.image}
-              />
-              <h1 className={styles.projectTitle}>{items.projectName}</h1>
-              <p className={styles.description}>{items.description}</p>
-              <a className={styles.view} href="#" target="_blank">
-                View Live
-              </a>
-              <a className={styles.source} href="#" target="_blank">
-                Github Repo
-              </a>
-            </div>
-          );
-        })}
+        {data && data.length === 0 ? (
+          <h1 className={styles.isProject}>Projects are Coming Soon</h1>
+        ) : (
+          data.map((items, index) => {
+            return (
+              <div className={styles.project} key={index}>
+                <img
+                  src={items.projectImg}
+                  alt={items.projectName}
+                  className={styles.image}
+                />
+                <h1 className={styles.projectTitle}>{items.projectName}</h1>
+                <p className={styles.description}>{items.description}</p>
+                <a className={styles.view} href="#" target="_blank">
+                  View Live
+                </a>
+                <a className={styles.source} href="#" target="_blank">
+                  Github Repo
+                </a>
+              </div>
+            );
+          })
+        )}
       </div>
     </section>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -3,19 +3,19 @@
  import todo from "../assets/todo.avif";
  
  export const data = [
-    {
-        projectName:"TodoApp",
-        projectImg: todo,
-        description:"This is the todo app"
-     },
-     {
-        projectName:"Calculator",
-        projectImg: calculator,
-        description:"This is the simple calculator app"
-     },
-     {
-        projectName:"User Authentication App",
-        projectImg:login,
-        description:"This is the simple  user authenticaion app"
-     }
+   //  {
+   //      projectName:"TodoApp",
+   //      projectImg: todo,
+   //      description:"This is the todo app"
+   //   },
+   //   {
+   //      projectName:"Calculator",
+   //      projectImg: calculator,
+   //      description:"This is the simple calculator app"
+   //   },
+   //   {
+   //      projectName:"User Authentication App",
+   //      projectImg:login,
+   //      description:"This is the simple  user authenticaion app"
+   //   }
 ];
